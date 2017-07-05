c0ca4bb80f4e8e9aa5b381c61fe67a2e2a6be923
Daniyar Itegulov <ditegulov@gmail.com>
2017-07-05 16:16:19 +0300

    Implement basic support of literals and local definitions

---
+++ interpreter/src/main/scala/org/scalameta/interpreter/internal/Engine.scala
@@ -0,0 +1,99 @@
+package org.scalameta.interpreter.internal
+
+import org.scalameta.interpreter.internal.environment.{Env, InterpreterRef}
+
+import scala.meta.Term.Block
+import scala.meta._
+
+object Engine {
+  def eval(tree: Tree, env: Env): (InterpreterRef, Env) = tree match {
+    case literal: Lit     => evalLiteral(literal, env)
+    case definition: Defn => evalLocalDef(definition, env)
+    case block: Block     => evalBlock(block, env)
+    case name: Term.Name  => evalName(name, env)
+  }
+
+  def evalName(name: Term.Name, env: Env): (InterpreterRef, Env) =
+    env.stack.head.get(name.value) match {
+      case Some(ref) => (ref, env)
+      case None      => sys.error(s"Unknown reference $name")
+    }
+
+  def evalBlock(block: Block, env: Env): (InterpreterRef, Env) = {
+    var result = InterpreterRef.wrap((), env, t"Unit")
+    for (stat <- block.stats) {
+      val (_, resEnv) = result
+      result = eval(stat, resEnv)
+    }
+    result
+  }
+
+  def evalLiteral(literal: Lit, env: Env): (InterpreterRef, Env) = literal match {
+    case Lit.Byte(value)    => InterpreterRef.wrap(value, env, t"Byte")
+    case Lit.Short(value)   => InterpreterRef.wrap(value, env, t"Short")
+    case Lit.Char(value)    => InterpreterRef.wrap(value, env, t"Char")
+    case Lit.Int(value)     => InterpreterRef.wrap(value, env, t"Int")
+    case Lit.Long(value)    => InterpreterRef.wrap(value, env, t"Long")
+    case Lit.Float(value)   => InterpreterRef.wrap(value, env, t"Float")
+    case Lit.Double(value)  => InterpreterRef.wrap(value, env, t"Double")
+    case Lit.Boolean(value) => InterpreterRef.wrap(value, env, t"Boolean")
+    case Lit.Unit(value)    => InterpreterRef.wrap(value, env, t"Unit")
+    case Lit.String(value)  => InterpreterRef.wrap(value, env, t"String")
+    case Lit.Null(value)    => InterpreterRef.wrap(value, env, t"Null")
+    case Lit.Symbol(value)  => sys.error(s"Can not interpret symbol tree $literal")
+    case _                  => sys.error(s"Can not interpret unrecognized tree $literal")
+  }
+
+  def defaultValue(tpe: Type, env: Env): (InterpreterRef, Env) = tpe match {
+    case t"Byte"    => InterpreterRef.wrap(0.toByte, env, tpe)
+    case t"Short"   => InterpreterRef.wrap(0.toShort, env, tpe)
+    case t"Char"    => InterpreterRef.wrap(0.toChar, env, tpe)
+    case t"Int"     => InterpreterRef.wrap(0, env, tpe)
+    case t"Long"    => InterpreterRef.wrap(0l, env, tpe)
+    case t"Float"   => InterpreterRef.wrap(0.0f, env, tpe)
+    case t"Double"  => InterpreterRef.wrap(0.0d, env, tpe)
+    case t"Boolean" => InterpreterRef.wrap(false, env, tpe)
+    case t"Unit"    => InterpreterRef.wrap((), env, tpe)
+    case _          => InterpreterRef.wrap(null, env, tpe)
+  }
+
+  def evalLocalDef(definition: Defn, env: Env): (InterpreterRef, Env) =
+    definition match {
+      case Defn.Val(mods, pats, _, rhs) =>
+        val (res, env1) = eval(rhs, env)
+        var resEnv      = env1
+        for (pat <- pats) {
+          pat match {
+            case Pat.Var.Term(name) =>
+              resEnv = resEnv.extend(name.value, res)
+          }
+        }
+        InterpreterRef.wrap((), resEnv, t"Unit")
+      case Defn.Var(mods, pats, optTpe, optRhs) =>
+        val (res, env1) = (optTpe, optRhs) match {
+          case (_, Some(rhs))    => eval(rhs, env)
+          case (Some(tpe), None) => defaultValue(tpe, env)
+          case (None, None)      => sys.error("Unreachable")
+        }
+        var resEnv = env1
+        for (pat <- pats) {
+          pat match {
+            case Pat.Var.Term(name) =>
+              resEnv = resEnv.extend(name.value, res)
+          }
+        }
+        InterpreterRef.wrap((), resEnv, t"Unit")
+      case Defn.Def(mods, name, tparams, paramss, tpe, body) =>
+        ???
+      case Defn.Trait(mods, name, tparams, ctor, templ) =>
+        ???
+      case Defn.Class(mods, name, tparams, ctor, templ) =>
+        ???
+      case Defn.Macro(mods, name, tparams, paramss, decltpe, body) =>
+        ???
+      case Defn.Object(mods, name, templ) =>
+        ???
+      case Defn.Type(mods, name, tparams, body) =>
+        ???
+    }
+}
