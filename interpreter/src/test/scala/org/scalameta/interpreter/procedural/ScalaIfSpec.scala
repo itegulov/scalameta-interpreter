5e0330c520a5f4cfba67e08e3fa3ea3423672c92
Daniyar Itegulov <ditegulov@gmail.com>
2017-07-11 19:45:01 +0300

    Add more tests for If expression

--- interpreter/src/test/scala/org/scalameta/interpreter/procedural/ScalaIfSpec.scala
+++ interpreter/src/test/scala/org/scalameta/interpreter/procedural/ScalaIfSpec.scala
@@ -11,4 +11,69 @@ class ScalaIfSpec extends ScalametaInterpreterSpec {
   it should "handle basic false if expression" in {
     checkCode(q"if (false) 1 else 2", 2, Seq())
   }
+
+  it should "handle comparison if expression" in {
+    checkCode(
+      q"""
+          val x = 1
+          if (x == 1) 1 else 2
+        """, 1, Seq())
+    checkCode(
+      q"""
+          val x = 1
+          if (x != 1) 1 else 2
+        """, 2, Seq())
+  }
+
+  it should "handle nested if expressions" in {
+    checkCode(
+      q"""
+          val x = 1
+          var y = 1
+          if (x == 1) {
+            y = 2
+            if (x != 2) {
+              y = 3
+            } else {
+              y = 4
+            }
+          } else {
+            y = 5
+          }
+          y
+        """, 3, Seq())
+    checkCode(
+      q"""
+          val x = 1
+          var y = 1
+          if (x == 1) {
+            y = 2
+            if (x != 1) {
+              y = 3
+            } else {
+              y = 4
+            }
+          } else {
+            y = 5
+          }
+          y
+        """, 4, Seq())
+  }
+
+  it should "handle if expression with mutable variables" in {
+    checkCode(
+      q"""
+          var x = 1
+          var y = 1
+          if (x == 1) {
+            x = 2
+            y = 2
+          }
+          if (x == 2) {
+            x = 3
+            y = 3
+          }
+          y
+        """, 3, Seq())
+  }
 }
