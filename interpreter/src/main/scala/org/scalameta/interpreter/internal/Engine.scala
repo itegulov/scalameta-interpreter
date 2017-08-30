c145cf4036f868047e1599a68c65687b57d03cf6
Daniyar Itegulov <ditegulov@gmail.com>
2017-08-30 16:46:04 +0300

    Add simple CLI
    (cherry picked from commit 24f42f8)

--- interpreter/src/main/scala/org/scalameta/interpreter/internal/Engine.scala
+++ interpreter/src/main/scala/org/scalameta/interpreter/internal/Engine.scala
@@ -36,6 +36,7 @@ object Engine {
     env: Env
   )(implicit mirror: ScalametaMirror): (InterpreterRef, Env) =
     tree match {
+      case source: Source     => evalSource(source, env)
       case literal: Lit       => evalLiteral(literal, env)
       case definition: Defn   => evalDefn(definition, env)
       case declaration: Decl  => InterpreterRef.wrap((), env, t"Unit") // FIXME: interpret them
@@ -77,6 +78,20 @@ object Engine {
       case select: Term.Select               => evalSelect(select, env)
       case xml: Term.Xml                     => sys.error("XMLs are unsupported")
     }
+  
+  def evalSource(
+    source: Source,
+    env: Env
+  )(implicit mirror: ScalametaMirror): (InterpreterRef, Env) = {
+    var resEnv = env
+    var resRef: InterpreterRef = InterpreterJvmRef(null)
+    for (stat <- source.stats) {
+      val (ref, newEnv) = eval(stat, resEnv)
+      resRef = ref
+      resEnv = newEnv
+    }
+    (resRef, resEnv)
+  }
 
   def evalDefn(
     definition: Defn,
